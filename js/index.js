649217908014078ab5f2e482b4419839e087efef
ShotaKiko <Shota.Kikozashvili@gmail.com>
2019-02-02 12:16:52 -0600

    Added event listeners for dbl clicking and mousing over in the navigation bar

--- js/index.js
+++ js/index.js
@@ -1,1 +1,33 @@
 // Your code goes here
+//EL signifies event listener
+
+
+// const homeAnchor = document.querySelector('.home-link')
+
+// homeAnchor.addEventListener('mouseover', event =>{
+//   console.log(`A link has been moused over.`)
+// })
+
+
+// ~~~~~~Navigation Bar ELs~~~~~~~~~~~~~~~~~~~
+
+//const navBarAnchors = document.querySelectorAll('.nav-link')
+const navBarAnchors = document.querySelector('.nav')
+    // console.log(navBarAnchors)
+const navContainer = document.querySelector('.nav-container')
+
+navBarAnchors.addEventListener('dblclick', event => {
+    console.log('dbl click is working')
+    navContainer.style.backgroundColor ="dodgerblue"
+    
+})
+navContainer.addEventListener('mouseover', event => {
+    console.log('mouseover is working')
+    navContainer.style.backgroundColor = "white"
+    event.stopPropagation()
+})
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
